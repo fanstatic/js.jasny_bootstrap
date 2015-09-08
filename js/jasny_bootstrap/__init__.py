f55adcde12f9bc8bf78457eb3ce1ad01e3565931
Richard Icke <ricke@minddistrict.com>
2015-09-08 12:29:04 +0200

    add css

--- js/jasny_bootstrap/__init__.py
+++ js/jasny_bootstrap/__init__.py
@@ -1,4 +1,4 @@
-from fanstatic import Library, Resource
+from fanstatic import Library, Resource, Group
 from js.jquery import jquery
 
 library = Library('jasny_bootstrap', 'resources')
@@ -7,8 +7,16 @@ library = Library('jasny_bootstrap', 'resources')
 # For options and examples, see the fanstatic documentation.
 # resource1 = Resource(library, 'style.css')
 
-jasny_bootstrap = Resource(
+jasny_bootstrap_css = Resource(
+    library,
+    'css/jasny-bootstrap.css'
+)
+
+jasny_bootstrap_js = Resource(
     library, 'js/jasny-bootstrap.js',
     depends=[jquery],
     minified='js/jasny-bootstrap.min.js'
 )
+
+jasny_bootstrap = Group(
+    [jasny_bootstrap_css, jasny_bootstrap_js])
